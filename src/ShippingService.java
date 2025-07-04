b8acb1a222998d22bc9bc0a138f9a329d110088e
Mohammed Hatem <mr.mohammedhatem@gmail.com>
2025-07-04 19:55:03 +0300

    Minor Modifications

--- src/ShippingService.java
+++ src/ShippingService.java
@@ -1,26 +1,30 @@
 import java.util.*;
 
 public class ShippingService {
-    public static void ship(List<Shippable> items) {
+    public static void ship(ArrayList<Shippable> items) {
         if (items.isEmpty()) return;
 
-        Map<String, Integer> counts = new TreeMap<>();
-        Map<String, Double> weights = new HashMap<>();
-        double totalWeight = 0.0;
+        // each distinct item is mapped to its number pf occurences and its weight
+        HashMap<String, Integer> counts = new HashMap<>();
+        HashMap<String,Double> weights = new HashMap<>();
+
+        double TotalWeight = 0;
+        String name;
 
         for (Shippable item : items) {
-            String name = item.getName();
+            name = item.getName();
             counts.put(name, counts.getOrDefault(name, 0) + 1);
             weights.put(name, item.getWeight());
-            totalWeight += item.getWeight();
+            TotalWeight += item.getWeight();
         }
-
+        //printing out each distinct item
         System.out.println("** Shipment notice **");
-        for (String name : counts.keySet()) {
-            int count = counts.get(name);
-            double weight = weights.get(name);
-            System.out.printf("%dx %s %.0fg\n", count, name, weight);
+        for(String item: counts.keySet()) {
+            System.out.printf("%dx %s %.0f\n", counts.get(item), item, weights.get(item));
         }
-        System.out.printf("Total package weight %.1fkg\n", totalWeight);
+        
+        System.out.printf("Total package weight %.1fkg\n", TotalWeight);
+
+
     }
 }
